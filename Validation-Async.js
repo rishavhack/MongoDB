33005338d9a361d8bc60aa19750f64ed9c95deac
rishavhack <rishav.jnit@gmail.com>
2021-10-15 13:32:28 +0530

    Created Validation for Async call

---
+++ Validation-Async.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+.then(() => console.log('Connected to Mongodb'))
+.catch(err => console.log('Could not connect to Mongodb..',err))
+
+const courseSchema = new mongoose.Schema({
+	name: { type:String, 
+		required:true,
+		minlength:5,
+		maxlength:255,
+		//match: /pattern/
+	},
+	category: {
+		type: String,
+		required:true,
+		enum: ['web','mobile','network']
+	},
+	author: String,
+	tags:{
+		type:Array,
+		validate: {
+			isAsync:true,
+			validator: function(v,callback) {
+				setTimeout(()=>{
+					const result = v && v.length > 0
+					callback(result);
+				},4000)
+				
+			},
+			message: 'A course should have at least one tag.'
+		}
+	},
+	date: {type : Date, default: Date.now},
+	isPublished: Boolean,
+	price: {
+		type : Number,
+		required: function() { return this.isPublished},
+		min:10,
+		max:200
+	}
+});
+
+const Course = mongoose.model('Course',courseSchema);
+async function createCourse() {
+	const course = new Course({
+	name: 'Backend DB Course',
+	category:'web',
+	author: 'Rishav',
+	tags: null,
+	isPublished :true,
+	price:10
+});
+
+	try {
+
+		const result = await course.save();
+		console.log("***",result);
+	} catch(ex) {
+		console.log(ex.message)
+	}
+
+}
+
+
+createCourse()
