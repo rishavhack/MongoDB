8cc1511f30ff5b5effae3bab7477ad1fb46102e5
rishavhack <rishav.jnit@gmail.com>
2021-11-01 08:58:41 +0530

    Model Creating DataBases

---
+++ population.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/population')
+.then(() => console.log('Connected to Mongodb'))
+.catch(err => console.log('Could not connect to Mongodb..',err))
+
+const Author = mongoose.model('Author',new mongoose.Schema({
+	name: String,
+	bio: String,
+	website: String
+}));
+
+const Course = mongoose.model('Course',new mongoose.Schema({
+	name: String,
+}));
+
+
+async function createAuthor(name,bio,website){
+	const author = new Author({
+		name,
+		bio,
+		website
+	});
+	const result = await author.save();
+	console.log(result);
+}
+
+async function createCourse(name,author){
+	const course = new Course({
+		name,
+		website
+	});
+	const result = await course.save();
+	console.log(result);
+}
+
+async function listCourses(){
+	const courses = await Course.find().select('name');
+	console.log(courses)
+}
+
+createAuthor('Mosh','My bio','My website');
